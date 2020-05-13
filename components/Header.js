9c45b70828457187698c6279b3dbda9416c59bc7
Lee Pedersen <leepedersenr@gmail.com>
2020-05-12 22:26:23 -0600

    start setting up ty site

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <div className="header">
+      <p className="title">Maxwell Prints</p>
+    </div>
+  )
+}
+
+export default Header;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Portfolio from '../components/Portfolio';
+import Header from '../components/Header';
+import Error404 from '../components/Error404';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Portfolio} />
+        <Route component={Error404} />
+      </Switch>
     </div>
   );
 }
