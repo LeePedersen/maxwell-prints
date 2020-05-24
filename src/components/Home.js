c5f9774ac06681fa684ebe9147ea4b84df3f1986
Lee Pedersen <leepedersenr@gmail.com>
2020-05-24 00:15:51 -0600

    add hover effects

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,11 @@ function Home() {
       <div className="header">
         <p className="homeTitle">Maxwell Prints</p>
       </div>
-      <span className='links'>Painting</span>
-      <span className='links'>Photography</span>
-      <span className='links'>Design</span>
+      <div className="links">
+        <span className='link'><a href="/painting">Painting</a></span>
+        <span className='link'><a href="/painting">Photography</a></span>
+        <span className='link'><a href="/painting">Design</a></span>
+      </div>
     </div>
   )
 }
