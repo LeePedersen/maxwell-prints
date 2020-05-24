551df75de7fe2c8038dc6a5c670566d9f18066f0
Lee Pedersen <leepedersenr@gmail.com>
2020-05-24 00:45:41 -0600

    mess with styling and onclick functionality

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ty_fish from './../assets/images/ty_fish.jpeg';
 
 function Painting() {
@@ -6,16 +6,32 @@ function Painting() {
   for (let i = 0; i < 20; i++) {
     images.push(ty_fish);
   }
+
+  const [imageState, setImageState] = useState('allImages');
+
+  function imagesClick() {
+    setImageState('bigImage');
+  }
+
+  function imageClick() {
+    setImageState('allImages');
+  }
+
   return(
     <div>
       <div className="header">
         <p className="title">Maxwell Prints</p>
       </div>
-      <div className='pictureScroll'>
+      { imageState === 'bigImage' && (
+        <div>
+        <img src={ty_fish} className="bigPic" onClick={imageClick}/>
+        </div>
+      )}
+        <div className="pictureScroll">
         {images.map((image, index) =>
-          <img src={image} className='fishPics'/>
+          <img src={image} className='fishPics' onClick={imagesClick}/>
         )}
-      </div>
+        </div>
     </div>
   )
 }
